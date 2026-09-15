f896cee45d19610c745404029f69786751ec37d9
dane foster <dene@slush.ca>
2026-09-15 19:35:57 +1200

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -184,8 +184,8 @@ smtp_secure_mode: $SMTP_SECURE_MODE
 smtp_verify_hostname: $SMTP_VERIFY_HOSTNAME
 smtp_verify_cert: $SMTP_VERIFY_CERT
 verp_confirmations: yes
-verp_personalized_deliveries: yes
-verp_delivery_interval: 1
+verp_personalized_deliveries: no
+verp_delivery_interval: 5
 max_recipients: 10
 max_delivery_threads: 10
 
